7477a18b5175d9784037ad1838d4ea8a340aa0c1
Admin\User <ducna>
2026-07-08 15:22:25 +0700

    chore(db): clear document data and export new seed_db.sql

--- seed_db.sql
+++ seed_db.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:22502b309a392792095728dfed7055566ced1a7a488033cef47efe2066d78ba6
-size 297342262
+oid sha256:c2d76918bcbd837bb59eaec19addf67edb73430915e99742673411a504c51dce
+size 99777
