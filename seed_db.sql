26ab6ab6d0a545962a98358b09d09ce1aedf015a
ducna <ducnguyen19971312@gmail.com>
2026-08-05 17:37:19 +0700

    feat(cabinet): cập nhật giao diện tạo phiên họp mới dạng wizard và bổ sung trường db

--- seed_db.sql
+++ seed_db.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:833468d5d56ad04681d72643690e78a72fc4b9aaac14e1f21dd8a5c5794e303b
-size 100826
+oid sha256:558e58bea5dd2a9ccfc84005bdbcf5254e14c761f4f756f410768a55b0a45d7e
+size 104118
